0405001efe95acb66a2954cb1c008dc8537f2e21
Senyo <simpsonsenyo@gmail.com>
2019-06-25 22:08:50 +0200

    dataset parsing & getting implemented

---
+++ sres/datasets/__init__.py
@@ -0,0 +1,1 @@
+from .pirm import PIRM
--- sres/datasets/pirm.py
+++ sres/datasets/pirm.py
@@ -2,8 +2,11 @@ import os
 import skimage
 import random
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 from glob import glob
+
+DIR_NAMES = ['4x_downsampled', 'Original']
 
 class PIRM(Dataset):
     def __init__(self, root, fmat='png', transform=None):
@@ -13,15 +16,20 @@ class PIRM(Dataset):
             fmat (str) : the format of the images in the dataset
         """
         ext = '*.%s' % fmat
-        self.image_paths = glob(os.path.join(root, ext))
+        self.lres_images = sorted(glob(os.path.join(root, DIR_NAMES[0], ext)))
+        self.hres_images = sorted(glob(os.path.join(root, DIR_NAMES[1], ext)))
+        self.dataset = list(zip(self.lres_images, self.hres_images))
         self.transform = transform
     
     def __getitem__(self, idx):
-        image_path = self.image_paths[idx]
-        sample = skimage.io.imread(image_path)
+        lres_img_path, hres_img_path = self.dataset[idx]
+        lres_img = skimage.io.imread(lres_img_path)
+        hres_img = skimage.io.imread(hres_img_path)
+        sample = (lres_img, hres_img)
+
         if self.transform:
             sample = self.transform(sample)
         return sample
 
     def __len__(self):
-        len(self.image_paths)
+        len(self.dataset)
