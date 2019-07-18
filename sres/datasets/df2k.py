5c89fac6f8a0a777b02a19a9167d823494a3097f
Senyo <simpsonsenyo@gmail.com>
2019-07-18 20:16:28 +0200

    df2k dataset added

--- sres/datasets/df2k.py
+++ sres/datasets/df2k.py
@@ -4,13 +4,13 @@ import torch
 from torch.utils.data import Dataset
 from glob import glob
 
-DIR_NAMES = ['LR', 'HR']
+DIR_NAMES = {'lr_dirname':'LR', 'hr_dirname':'HR'}
 
 class DF2K(Dataset):
     def __init__(self, root, fmat='png', transform=None):
         self.root = root
         self.ext = '*.%s' % fmat
-        self.hres_images = glob(os.path.join(self.root, DIR_NAMES[1], self.ext))
+        self.hres_images = glob(os.path.join(self.root, DIR_NAMES['hr_dirname'], self.ext))
         self.lres_images = self._get_lr_paths(self.hres_images)
         self.dataset = list(zip(self.lres_images, self.hres_images))
         self.transform = transform
@@ -21,7 +21,7 @@ class DF2K(Dataset):
             root, ext = os.path.splitext(path)
             basename = os.path.basename(root)
             lr_filename = '%sx4%s' % (basename, ext)
-            lr_path = os.path.join(self.root, DIR_NAMES[0], lr_filename)
+            lr_path = os.path.join(self.root, DIR_NAMES['lr_dirname'], lr_filename)
             lr_paths.append(lr_path)
         return lr_paths
 
