7bfcc18c996c275496ab413012561a02800f3b9c
Senyo <simpsonsenyo@gmail.com>
2019-07-19 14:04:05 +0200

    correct implementation

--- sres/models/srgan.py
+++ sres/models/srgan.py
@@ -36,16 +36,22 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_shape):
+    def __init__(self, input_shape, batch_size=16):
         super().__init__()
         self.name = 'SRGAN'
+        self.batch_size = batch_size
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         strides = [2, 1, 2, 1, 2, 1, 2]
-        channels = [(3,64), (64,128), (128,128), (128,256), (256,256), (256,512), (512,512)]
-        self.blocks = self._make_layer(DiscriminatorBlock, strides, channels)
+        channels = [(64,64), (64,128), (128,128), (128,256), (256,256), (256,512), (512,512)]
+        self.blocks = self._make_layer(DiscriminatorBlock, channels, strides)
 
-        n_input = int((np.prod(input_shape) * 512) / 16)
+        n_downsize = sum([1 for stride in strides if stride == 2])
+        output_shape = int(input_shape / (2 ** n_downsize))
+        
+        if isinstance(input_shape, int):
+            output_shape = (output_shape, output_shape)
+        n_input = int(np.prod(output_shape) * 512)
         self.affine1 = nn.Linear(n_input, 1024)
         self.affine2 = nn.Linear(1024, 1)
 
@@ -55,11 +61,11 @@ class Discriminator(nn.Module):
     def forward(self, x):
         x = self.leakyrelu(self.conv1(x))
         x = self.blocks(x)
-        x = x.view(-1, 1024)
+        x = x.view(self.batch_size, -1)
         x = self.leakyrelu(self.affine1(x))
-        answer = self.sigmoid(self.affine2(x))
-        return answer
+        pred = self.sigmoid(self.affine2(x))
+        return pred
 
-    def _make_layer(self, block, strides, channels):
-        layer = [block(*channel, stride=stride) for channel, stride in zip(strides, channels)]
+    def _make_layer(self, block, channels, strides):
+        layer = [block(*channel, stride=stride) for channel, stride in zip(channels, strides)]
         return nn.Sequential(*layer)
