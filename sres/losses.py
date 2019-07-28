89b09c13ab82ddda6b6a06fa271a537bb24cd7ca
Senyo <simpsonsenyo@gmail.com>
2019-07-28 18:31:47 +0200

    implemented vgg from esrgan

--- sres/losses.py
+++ sres/losses.py
@@ -26,10 +26,13 @@ class PerceptualLoss(nn.Module):
         self.content_loss = self.content_losses[content_loss_type]()
         self.adversarial_loss = AdversarialLoss()
         self.bce = BCELoss()
+        self.mse = MSELoss()
+        self.alpha = 1e-2
         self.beta = 10e-3
 
     def forward(self, output, target, probs, target_probs):
         loss = self.content_loss(output, target) + \
+            self.alpha * self.mse(output, target) + \
             self.beta * self.bce(probs, target_probs)
         return loss
 
@@ -37,14 +40,20 @@ class PerceptualLoss(nn.Module):
 class VGGLoss(nn.Module):
     def __init__(self, layer=34):
         super().__init__()
+        self.device = torch.device('cuda' if use_cuda else 'cpu')
         self.vgg19 = vgg19(pretrained=True)
         self.feature_extractor = nn.Sequential(*list(self.vgg19.features.children()))[:layer+1]
         for k, v in self.feature_extractor.named_parameters():
             v.requires_grad = False
         self.mse = nn.MSELoss()
-
+        self.mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(self.device)
+        self.std = torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(self.device)
+    
     def forward(self, gen_hr, real_hr, beta=0.006):
         gen_hr_feat_map = self.feature_extractor(gen_hr)
+        gen_hr_feat_map = (gen_hr_feat_map - self.mean) / self.std
         real_hr_feat_map = self.feature_extractor(real_hr)
-        loss = beta * self.mse(gen_hr_feat_map, real_hr_feat_map)
+        real_hr_feat_map = (real_hr_feat_map - self.mean) / self.std
+
+        loss = self.mse(gen_hr_feat_map, real_hr_feat_map)
         return loss
