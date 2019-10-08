f954e5745598a005a91df310141017dc1f853bb6
Senyo <simpsonsenyo@gmail.com>
2019-10-08 22:49:03 +0200

    added msrresnet import

--- sres/models/__init__.py
+++ sres/models/__init__.py
@@ -1,4 +1,4 @@
-from .srresnet import SRResNet
+from .srresnet import SRResNet, MSRResNet
 from .srgan import Generator as SRGenerator
 from .srgan import Discriminator as SRDiscriminator
 from .esrgan import Generator as ESRGenerator
