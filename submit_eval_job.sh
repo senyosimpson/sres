e7788c93b3d5805da83e604dd071000fedb99185
Senyo <simpsonsenyo@gmail.com>
2019-10-10 21:04:04 +0200

    eval job submit

---
+++ submit_eval_job.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+gradient jobs create \
+--name "$1" \
+--container ${CONTAINER_NAME} \
+--registryUsername ${DOCKERHUB_USERNAME} \
+--registryPassword ${DOCKERHUB_PASSWORD} \
+--machineType "P5000" \
+--command "./eval.sh" \
+--ignoreFiles "weights","checkpoint"
