84cd285c5852b87f793b92c5f93be3321d2ad5e1
Senyo <simpsonsenyo@gmail.com>
2019-10-10 21:03:48 +0200

    eval script complete

--- sres/eval.py
+++ sres/eval.py
@@ -3,10 +3,11 @@ import argparse
 import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
-from sres.transforms import ToTensor, Normalize, Transpose, \
-    RandomCrop, RandomHorizontalFlip, RandomRotateNinety
-from sres.constants import MODELS, DATASETS
-from sres.metrics import SSIM, PSNR
+from sres.transforms import ToTensor
+from sres.constants import MODELS
+from sres.constants import DATASETS
+from sres.metrics import SSIM
+from sres.metrics import PSNR
 
 
 if __name__ == '__main__':
@@ -34,11 +35,6 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help='the path to the root directory of the dataset')
-    parser.add_argument('--colour-space',
-                        type=str,
-                        default='rgb',
-                        required=False,
-                        help='the colour space to evaluate on')
     parser.add_argument('--batch-size',
                         type=int,
                         default=1,
@@ -50,26 +46,35 @@ if __name__ == '__main__':
                         required=False,
                         help='number of workers used in loading data')
     args = parser.parse_args()
+    model = args.model
+    model_path = args.model_path
+    root_dir = args.root_dir
+    dataset = args.dataset
+    batch_size = args.batch_size
+    num_workers = args.num_workers
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    use_cuda = not False and torch.cuda.is_available()
-    device = torch.device('cuda' if use_cuda else 'cpu')
+    # make a utility function called get_attr
+    if model == 'srgan':
+        weights = torch.load(model_path)['generator_state_dict']
+        model = MODELS[model][0]
+    else:
+        weights = torch.load(model_path)['model_state_dict']
+        model = MODELS[model]
 
-    weights = torch.load(args.model_path)['model_state_dict']
-    model = MODELS[args.model]
-    model = model()
+    model = model().to(device)
     model.load_state_dict(weights)
-    model.to(device)
-    
+
     tsfm = Compose([
         ToTensor()
     ])
 
-    ds = DATASETS[args.dataset]
-    dataset = ds(args.root_dir, transform=tsfm)
+    ds = DATASETS[dataset]
+    dataset = ds(root_dir, transform=tsfm)
     dataloader = DataLoader(dataset,
                             shuffle=False,
-                            batch_size=args.batch_size,
-                            num_workers=args.num_workers)
+                            batch_size=batch_size,
+                            num_workers=num_workers)
 
     psnr = PSNR()
     ssim = SSIM()
@@ -78,10 +83,19 @@ if __name__ == '__main__':
     model.eval()
     with torch.no_grad():
         avg_psnr = 0
+        avg_ssim = 0
         for idx, image_pair in enumerate(dataloader):
             logger.info('Batch %d/%d' % (idx+1, len(dataloader)))
             lr_image, hr_image = image_pair
+            lr_image = lr_image.to(device)
+            hr_image = hr_image.to(device)
             generated_img = model(lr_image)
+            # crop border pixels
+            hr_image = hr_image[:, :, 4:-4, 4:-4]
+            generated_img = generated_img[:, :, 4:-4, 4:-4]
+            # evaluate metrics
             avg_psnr += psnr(generated_img, hr_image)
+            avg_ssim += ssim(generated_img, hr_image)
     
     print("Avg. PSNR: {:.4f} dB".format(avg_psnr / len(dataloader)))
+    print("Avg. SSIM: {:.4f} dB".format(avg_ssim / len(dataloader)))
