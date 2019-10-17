9edd064c6410c01adfcb9c2a32a9e05d914dab2e
Senyo <simpsonsenyo@gmail.com>
2019-10-17 14:18:08 +0200

    uses msrresnet

--- sres/models/srgan.py
+++ sres/models/srgan.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
+from functools import partial
 from .submodules import *
 
 
@@ -8,26 +10,31 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.name = 'SRGAN'
+        
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv3 = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1, bias=True)
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4, bias=False)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv3 = nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4, bias=False)
+        res_block = partial(ResidualBlock, use_bn=False, bias=True, shortcut=None)
+        self.residual = self._make_layer(res_block, 16)
+        self.sub_pixel_conv1 = SubPixelConv2d(64, 256, bias=True)
+        self.sub_pixel_conv2 = SubPixelConv2d(64, 256, bias=True)
 
-        self.residual = self._make_layer(ResidualBlock, num_blocks=16)
-        self.sub_pixel_conv1 = SubPixelConv2d(64, 256)
-        self.sub_pixel_conv2 = SubPixelConv2d(64, 256)
-
-        self.bn = nn.BatchNorm2d(64)
         self.prelu = nn.PReLU()
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
-        input_layer = self.prelu(self.conv1(x))
-        x = self.residual(input_layer)
-        x = self.bn(self.conv2(x))
-        x = torch.add(x, input_layer)
+        base = x
+        x = self.prelu(self.conv1(x))
+        x = self.residual(x)
         x = self.sub_pixel_conv1(x)
         x = self.sub_pixel_conv2(x)
-        hres = self.conv3(x)
+        x = self.prelu(self.conv2(x))
+        x = self.conv3(x)
+        base = F.interpolate(base, scale_factor=4, mode='bicubic')
+        base = torch.clamp(base, min=0., max=1.)
+        hres = x + base
+        hres = (self.tanh(hres) + 1) / 2
         return hres
 
     def _make_layer(self, block, num_blocks):
